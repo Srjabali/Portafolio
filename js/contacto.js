43aeef101ef4e005393dc9e0c815e655f8ac8b2e
Victor <varc.89@hotmail.com>
2022-11-03 10:38:40 -0300

    Finalizando with mode responsive

--- js/contacto.js
+++ js/contacto.js
@@ -1,9 +1,40 @@
-const $form = document.querySelector('#form')
+const $form = document.querySelector('#form');
 
-$form.addEventListener('submit', handleSubmit)
+$form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event){
     event.preventDefault()
     alert("wena loco")
 
 }
+
+const burgerBtn = document.querySelector('#burger_btn');
+const exeBtn= document.querySelector('#exe_btn');
+const header = document.querySelector('.header');
+const body = document.querySelector('#body');
+const nav = document.querySelector("#nav")
+
+
+burgerBtn.addEventListener('click', () =>{
+    body.classList.toggle("aparece-body")
+    header.classList.toggle("aparece-header");
+    exeBtn.classList.toggle("aparece-exe")
+    burgerBtn.classList.toggle("aparece-burger")
+    
+});
+
+exeBtn.addEventListener('click', ()=>{
+    body.classList.toggle("aparece-body")
+    header.classList.toggle("aparece-header");
+    exeBtn.classList.toggle("aparece-exe")
+    burgerBtn.classList.toggle("aparece-burger")
+    
+});
+
+nav.addEventListener('click', ()=>{
+    body.classList.toggle("aparece-body")
+    header.classList.toggle("aparece-header");
+    exeBtn.classList.toggle("aparece-exe")
+    burgerBtn.classList.toggle("aparece-burger")
+    
+})
